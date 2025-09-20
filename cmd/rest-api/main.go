3ba0c2e23e60674e49ce8098ab6b2fd14dd65f90
atharvarekhawar <atharvarekhawar64@gmail.com>
2025-09-20 15:43:54 +0530

    server started

--- cmd/rest-api/main.go
+++ cmd/rest-api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"rest-api/internal/config"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Print("fwfew")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
+	cfg := config.SerializeConfig()
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to server 3000"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("Server started")
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
+
 }
